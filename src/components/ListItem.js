c5ca589d402dd926d5170059518f3ba61b890744
XUHO3 <howells.xu@oocl.com>
2018-07-20 17:28:54 +0800

    complete

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -23,11 +23,11 @@ export default class ListItem extends Component {
     render() {
         const {todo, editItem, checkItem} = this.props;
         return (
-            <div>
+            <ol>
                 {todo.map(x => <li id={x.id} className={x.name} onDoubleClick={() => editItem(x.id)}>
                     <input name="done-todo" type="checkbox" checked={x.name===""?false:true} className="done-todo" onClick={() => checkItem(x.id)}/>
                     <span contentEditable={x.contentEditable}> {x.value}</span></li>)}
-            </div>
+            </ol>
         )
     }
 }
